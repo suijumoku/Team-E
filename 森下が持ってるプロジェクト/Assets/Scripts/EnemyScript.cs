42c324b2e9a0f178983b9f7a115a24bd27744b0a
shigeta0506 <g2227042@st.t-kougei.ac.jp>
2023-10-17 17:10:40 +0900

    別のファイルのやつ移した

---
+++ 森下が持ってるプロジェクト/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,99 @@
+using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Transform child;
+    [SerializeField] Material patrolMaterial;
+    [SerializeField] Material chaseMaterial;
+    [SerializeField] float detectDistance;
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    bool IsDetected = false;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        // autoBraking を無効にすると、目標地点の間を継続的に移動します
+        //(つまり、エージェントは目標地点に近づいても
+        // 速度をおとしません)
+        agent.autoBraking = false;
+
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        // 地点がなにも設定されていないときに返します
+        if (points.Length == 0)
+            return;
+
+        // エージェントが現在設定された目標地点に行くように設定します
+        agent.destination = points[destPoint].position;
+
+        // 配列内の次の位置を目標地点に設定し、
+        // 必要ならば出発地点にもどります
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+        float distance;
+
+        distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance < detectDistance)
+        {
+            if (!IsDetected)
+            {
+                GetComponent<Renderer>().material = chaseMaterial;
+            }
+            IsDetected = true;
+        }
+        else
+        {
+            if (IsDetected)
+            {
+                GetComponent<Renderer>().material = patrolMaterial;
+            }
+            IsDetected = false;
+        }
+
+        if (!IsDetected)
+        {
+            // エージェントが現目標地点に近づいてきたら、
+            // 次の目標地点を選択します
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                GotoNextPoint();
+            }
+        }
+        else
+        {
+            agent.destination = player.position;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            child.gameObject.AddComponent<NavMeshAgent>();
+
+            //親子関係解除
+            transform.DetachChildren();
+
+            //オブジェクトの消去
+            Destroy(gameObject);
+
+        }
+    }
+
+}
