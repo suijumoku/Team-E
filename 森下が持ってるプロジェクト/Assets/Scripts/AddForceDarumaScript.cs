a71c4223ac1cd21df967cf8e93084f25a1026553
shigeta0506 <g2227042@st.t-kougei.ac.jp>
2023-11-07 13:54:14 +0900

    手直し

--- 森下が持ってるプロジェクト/Assets/Scripts/AddForceDarumaScript.cs
+++ 森下が持ってるプロジェクト/Assets/Scripts/AddForceDarumaScript.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -35,42 +34,4 @@ public class AddForceDarumaScript : MonoBehaviour
 
         }
     }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class AddForceDarumaScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Ball")
-        {
-            float boundsPower = 10.0f;
-
-            // 衝突位置を取得する
-            Vector3 hitPos = collision.contacts[0].point;
-
-            // 衝突位置から自機へ向かうベクトルを求める
-            Vector3 boundVec = this.transform.position - hitPos;
-
-            // 逆方向にはねる
-            Vector3 forceDir = boundsPower * boundVec.normalized;
-            this.GetComponent<Rigidbody>().AddForce(forceDir, ForceMode.Impulse);
-
-        }
-    }
->>>>>>> 93259010dd9cca53a340056aae408d0fbac9161c
 }
