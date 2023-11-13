5054cdd4c932bb83d9a89c2846f617eb10f232a5
shigeta0506 <g2227042@st.t-kougei.ac.jp>
2023-11-13 18:31:17 +0900

    修正

--- 森下が持ってるプロジェクト/Assets/Scripts/DarumaHeadScript.cs
+++ 森下が持ってるプロジェクト/Assets/Scripts/DarumaHeadScript.cs
@@ -7,7 +7,6 @@ public class DarumaHeadScript : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] ResultManager resultManager;
-   //[SerializeField] SpownenemyScript spownenemyScript;
     void Start()
     {
         
