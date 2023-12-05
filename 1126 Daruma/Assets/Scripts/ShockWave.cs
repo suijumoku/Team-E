6849e41aff8f0efbd65b75512dba3f261413af23
suijumoku <annryuuki@gmail.com>
2023-12-05 11:41:54 +0900

    ボスだるま

--- 1126 Daruma/Assets/Scripts/ShockWave.cs
+++ 1126 Daruma/Assets/Scripts/ShockWave.cs
@@ -1,32 +1,32 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEditor.ProBuilder;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ProBuilder;
+using UnityEngine;
 
-//public class ShockWave : MonoBehaviour
-//{
-//    [Header("ŽË’ö”¼Œa")] public float LengthRadius;
-//    [Header("ŽžŠÔ")] public float MoveTime = 2f;
-//    float time;
-//    float Move;
+public class ShockWave : MonoBehaviour
+{
+    [Header("ŽË’ö”¼Œa")] public float LengthRadius;
+    [Header("ŽžŠÔ")] public float MoveTime = 2f;
+    float time;
+    float Move;
 
-//    private void Start()
-//    {
-//        Move = LengthRadius / MoveTime;
-//    }
-//    private void Update()
-//    {
-//        Vector3 scl = transform.localScale;
-//        time += Time.deltaTime;
-//        if (time < MoveTime)
-//        {
-//            scl.x += Move * Time.deltaTime;
-//            scl.z += Move * Time.deltaTime;
-//            transform.localScale = scl;
-//        }
-//        else
-//        {
-//            Destroy(gameObject);
-//        }
-//    }
-//}
+    private void Start()
+    {
+        Move = LengthRadius / MoveTime;
+    }
+    private void Update()
+    {
+        Vector3 scl = transform.localScale;
+        time += Time.deltaTime;
+        if (time < MoveTime)
+        {
+            scl.x += Move * Time.deltaTime;
+            scl.z += Move * Time.deltaTime;
+            transform.localScale = scl;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
