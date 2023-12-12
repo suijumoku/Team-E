66fd05a7a70c00b6e373d1cf0a654d0ea53ab753
suijumoku <annryuuki@gmail.com>
2023-12-12 12:42:19 +0900

    スタート時のカウントダウン BGMループ

---
+++ 1126 Daruma/Assets/Scripts/UI/StartCountDown.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCountDown : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    [Header("再生するアニメーションクリップ")]
+    private AnimationClip clip;
+    private Animator animator;
+
+    private void Start()
+    {
+        Time.timeScale = 0f;
+        animator =GetComponent<Animator>();
+        string name = clip.name;
+        animator.Play(name);
+    }
+    public void OnAnimationEnd()
+    {
+        Time.timeScale = 1.0f;
+        Destroy(gameObject);
+    }
+}
