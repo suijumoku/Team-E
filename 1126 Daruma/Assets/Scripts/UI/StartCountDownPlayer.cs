63735360fa06e29d83bb916d562ed317c46e2608
suijumoku <annryuuki@gmail.com>
2024-01-09 02:38:19 +0900

    終了時の演出を追加した

--- 1126 Daruma/Assets/Scripts/UI/StartCountDownPlayer.cs
+++ 1126 Daruma/Assets/Scripts/UI/StartCountDownPlayer.cs
@@ -1,9 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class StartCountDown : MonoBehaviour
+public class StartCountDownPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
